7f51fdf0f706e40206d24482f2ae9b16ab5d34d1
Anish Raj Anand <anishrajanand@Anishs-MacBook-Air.local>
2023-03-07 15:03:39 +0530

    first commit for NFT collection

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x75eB11e4d01a6a7890b569597D6FF5821280426F";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
